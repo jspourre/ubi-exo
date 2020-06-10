f86ef5f068596d48f3583b5d41783bf5deafd05d
jspourre <jspourre@gmail.com>
2020-06-10 16:52:58 +0200

    ajoout de contraite avec assert

--- src/Entity/Eleve.php
+++ src/Entity/Eleve.php
@@ -7,6 +7,7 @@ use App\Repository\EleveRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource()
@@ -23,16 +24,22 @@ class Eleve
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Type("string")
      */
     private $nom;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Type("string")
      */
     private $prenom;
 
     /**
      * @ORM\Column(type="date")
+     * @Assert\NotBlank
+     * @Assert\Date
      */
     private $dateNaissance;
 
